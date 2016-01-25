62457969fa6e08a2df404b2ed8cd050fb941bf03
Emilio Barrera Sepúlveda <barrera_emilio@yahoo.es>
2016-01-25 18:50:33 +0100

    DProveedor - Terminado

--- CapaDatos/DProveedor.cs
+++ CapaDatos/DProveedor.cs
@@ -172,9 +172,249 @@ namespace CapaDatos
             this.TextoBuscar = textoBuscar;
         }
 
-        //hola emilio don chimbo....
+        //Métodos:
+        //Método Insertar Proveedor: es publico para que se pueda comunicar con las demas capas...
+        public string Insertar(DProveedor proveedor)
+        {
+            string respuesta = "";
+            SqlConnection SqlCon = new SqlConnection();
+            try
+            {
+                SqlCon.ConnectionString = Conexion.Cn;
+                SqlCon.Open();
+                //Establecer el Comando:
+                SqlCommand cmd = new SqlCommand("spInsertarProvedor", SqlCon);
+                cmd.CommandType = CommandType.StoredProcedure;
+                //Parametros:
+                SqlParameter pIdProveedor = new SqlParameter("@IdProveedor", proveedor.IdProveedor);
+                cmd.Parameters.Add(@IdProveedor);
+
+                SqlParameter pRazonSocial = new SqlParameter("@RazonSocial", proveedor.RazonSocial);
+                cmd.Parameters.Add(pRazonSocial);
+
+                SqlParameter pSectorComercial = new SqlParameter("@SectorComercial", proveedor.SectorComercial);
+                cmd.Parameters.Add(pSectorComercial);
+
+                SqlParameter pTipoDocumento = new SqlParameter("@TipoDocumento", proveedor.TipoDocumento);
+                cmd.Parameters.Add(pTipoDocumento);
+
+                SqlParameter pNumeroDocumento = new SqlParameter("@NumeroDocumento", proveedor.NumeroDocumento);
+                cmd.Parameters.Add(pNumeroDocumento);
+
+                SqlParameter pDireccion = new SqlParameter("@Direccion", proveedor.Direccion);
+                cmd.Parameters.Add(pSectorComercial);
+
+                SqlParameter pTelefono = new SqlParameter("@Telefono", proveedor.Telefono);
+                cmd.Parameters.Add(pTelefono);
+
+                SqlParameter pEmail = new SqlParameter("@Email", proveedor.Email);
+                cmd.Parameters.Add(pEmail);
+
+                SqlParameter pUrl = new SqlParameter("@Url", proveedor.Url);
+                cmd.Parameters.Add(pUrl);
+
+                //Ejecutamos nuestro comando:
+                respuesta = cmd.ExecuteNonQuery() == 1 ? "OK" : "No se ingres el Resgistro.....";
 
 
+            }
+            catch (Exception ex)
+            {
+
+                respuesta = "ERROR: " + ex.Message;
+            }
+            finally
+            {
+                if (SqlCon.State == ConnectionState.Open)
+                {
+                    SqlCon.Close();
+                }
+            }
+
+            return respuesta;
+        }
+
+        //Método editat:
+        public string Editar(DProveedor proveedor)
+        {
+            string respuesta = "";
+            SqlConnection SQlCon = new SqlConnection();
+            try
+            {
+                SQlCon.ConnectionString = Conexion.Cn;
+                SQlCon.Open();
+                //Establecer comando:
+                SqlCommand cmd = new SqlCommand("spEditarProvedor", SQlCon);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                //Parametros:
+                SqlParameter pIdProveedor = new SqlParameter("@IdProveedor", proveedor.IdProveedor);
+                cmd.Parameters.Add(@IdProveedor);
+
+                SqlParameter pRazonSocial = new SqlParameter("@RazonSocial", proveedor.RazonSocial);
+                cmd.Parameters.Add(pRazonSocial);
+
+                SqlParameter pSectorComercial = new SqlParameter("@SectorComercial", proveedor.SectorComercial);
+                cmd.Parameters.Add(pSectorComercial);
+
+                SqlParameter pTipoDocumento = new SqlParameter("@TipoDocumento", proveedor.TipoDocumento);
+                cmd.Parameters.Add(pTipoDocumento);
+
+                SqlParameter pNumeroDocumento = new SqlParameter("@NumeroDocumento", proveedor.NumeroDocumento);
+                cmd.Parameters.Add(pNumeroDocumento);
+
+                SqlParameter pDireccion = new SqlParameter("@Direccion", proveedor.Direccion);
+                cmd.Parameters.Add(pSectorComercial);
+
+                SqlParameter pTelefono = new SqlParameter("@Telefono", proveedor.Telefono);
+                cmd.Parameters.Add(pTelefono);
+
+                SqlParameter pEmail = new SqlParameter("@Email", proveedor.Email);
+                cmd.Parameters.Add(pEmail);
+
+                SqlParameter pUrl = new SqlParameter("@Url", proveedor.Url);
+                cmd.Parameters.Add(pUrl);
+
+                //Ejecuto el comando:
+
+                respuesta = cmd.ExecuteNonQuery() == 1 ? "OK" : "No se Actualizo el Registro.....";
+            }
+            catch (Exception ex)
+            {
+
+                respuesta = "ERROR: " + ex.Message;
+            }
+            finally
+            {
+                if (SQlCon.State == ConnectionState.Open)
+                {
+                    SQlCon.Close();
+                }
+            }
+
+            return respuesta;
+        }
+
+        //Método Eliminar
+
+        public string Eliminar(DProveedor proveedor)
+        {
+            string respuesta = string.Empty;
+            SqlConnection SqlCon = new SqlConnection();
+            try
+            {
+                SqlCon.ConnectionString = Conexion.Cn;
+                SqlCon.Open();
+                //Comando:
+                SqlCommand cmd = new SqlCommand("spEliminarProveedor", SqlCon);
+                cmd.CommandType = CommandType.StoredProcedure;
+                //parametros:
+                SqlParameter pIdCategoria = new SqlParameter("@IdProveedor", proveedor.IdProveedor);
+                cmd.Parameters.Add(pIdCategoria);
+
+                respuesta = cmd.ExecuteNonQuery() == 1 ? "OK" : "NO Se Elimino el Registro.....";
+
+            }
+            catch (Exception ex)
+            {
+
+                respuesta = "ERROR: " + ex.Message;
+            }
+            finally
+            {
+                if (SqlCon.State == ConnectionState.Open)
+                {
+                    SqlCon.Close();
+                }
+            }
+
+            return respuesta;
+        }
+
+        //Meétodo mostrar todos los valores de proveedor:
+        public DataTable MostrarProveedor()
+        {
+            DataTable dataTableResultado = new DataTable("proveedor");
+            SqlConnection sqlCon = new SqlConnection();
+            try
+            {
+                sqlCon.ConnectionString = Conexion.Cn;
+                sqlCon.Open();
+                //comando;
+                SqlCommand cmd = new SqlCommand("spMostrarProveedor", sqlCon);
+                cmd.CommandType = CommandType.StoredProcedure;
+                //dataAdapter
+                SqlDataAdapter Da = new SqlDataAdapter(cmd);
+                Da.Fill(dataTableResultado);
+            }
+            catch (Exception)
+            {
+
+                dataTableResultado = null;
+            }
+            sqlCon.Close();
+            return dataTableResultado;
+        }
+
+        //Método buscar ProveedorNumeroDocumento de las Proveedores:
+        public DataTable BuscarProveedorNumeroDocumento(DProveedor proveedor)
+        {
+            DataTable dataTableResultado = new DataTable("proveedor");
+            SqlConnection sqlCon = new SqlConnection();
+            try
+            {
+                sqlCon.ConnectionString = Conexion.Cn;
+                sqlCon.Open();
+                //comando:
+                SqlCommand cmd = new SqlCommand("spBuscarProveedorNumeroDocumento", sqlCon);
+                cmd.CommandType = CommandType.StoredProcedure;
+                //parametros:
+                SqlParameter ptxtBuscar = new SqlParameter("@txtBuscar", proveedor.TextoBuscar);
+                cmd.Parameters.Add(ptxtBuscar);
+                //DataAdaptar:
+                SqlDataAdapter Da = new SqlDataAdapter(cmd);
+                Da.Fill(dataTableResultado);
+
+            }
+            catch (Exception)
+            {
+
+                dataTableResultado = null;
+            }
+
+            sqlCon.Close();
+            return dataTableResultado;
+        }
+
+        //Método buscar  BuscarProveedorRazonSocia de las Proveedores:
+        public DataTable BuscarProveedorRazonSocial(DProveedor proveedor)
+        {
+            DataTable dataTableResultado = new DataTable("proveedor");
+            SqlConnection sqlCon = new SqlConnection();
+            try
+            {
+                sqlCon.ConnectionString = Conexion.Cn;
+                sqlCon.Open();
+                //comando:
+                SqlCommand cmd = new SqlCommand("spBuscarProveedorRazonSocial", sqlCon);
+                cmd.CommandType = CommandType.StoredProcedure;
+                //parametros:
+                SqlParameter ptxtBuscar = new SqlParameter("@txtBuscar", proveedor.TextoBuscar);
+                cmd.Parameters.Add(ptxtBuscar);
+                //DataAdaptar:
+                SqlDataAdapter Da = new SqlDataAdapter(cmd);
+                Da.Fill(dataTableResultado);
+
+            }
+            catch (Exception)
+            {
+
+                dataTableResultado = null;
+            }
+
+            sqlCon.Close();
+            return dataTableResultado;
+        }
 
     }
 }
