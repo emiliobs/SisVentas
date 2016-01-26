2b75e66c20e96b5869388c0c6a538a89ed950ac1
Emilio Barrera Sepúlveda <barrera_emilio@yahoo.es>
2016-01-26 23:04:05 +0100

    configurado con - frmProveedor

--- SisVentas/Program.cs
+++ SisVentas/Program.cs
@@ -18,7 +18,7 @@ namespace SisVentas
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(frmArticulo.GetInstancia());
+            Application.Run(new  frmProveedor());
         }
     }
 }
