50219ae355738ee16654c029e84664d7744660e0
Emilio Barrera Sepúlveda <barrera_emilio@yahoo.es>
2016-02-02 22:35:19 +0100

    DIngresoDetalle - en Desarrollo

--- CapaDatos/DDetalleIngreso.cs
+++ CapaDatos/DDetalleIngreso.cs
@@ -3,10 +3,155 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//
+using System.Data;
+using System.Data.SqlClient;
 namespace CapaDatos
 {
-    class DDetalleIngreso
+   public class DDetalleIngreso
     {
+        //variables:
+        private int idDetalleIngreso;
+        private int idIngreso;
+        private int idArticulo;
+        private decimal precioCompra;
+        private decimal precioVenta;
+        private int stockInicial;
+        private int stockActual;
+        private DateTime fechaProduccion;
+        private DateTime fechaVencimiento;
+
+        public DDetalleIngreso()
+        { }
+
+        public DDetalleIngreso(int idDetalleIngreso, int idIngreso, int idArticulo, decimal precioCompra, decimal precioVenta, int stockInicial, int stockActual, DateTime fechaProduccion, DateTime fechaVencimiento)
+        {
+            this.idDetalleIngreso = idDetalleIngreso;
+            this.idIngreso = idIngreso;
+            this.idArticulo = idArticulo;
+            this.precioCompra = precioCompra;
+            this.precioVenta = precioVenta;
+            this.stockInicial = stockInicial;
+            this.stockActual = stockActual;
+            this.fechaProduccion = fechaProduccion;
+            this.fechaVencimiento = fechaVencimiento;
+        }
+
+        public int IdDetalleIngreso
+        {
+            get
+            {
+                return idDetalleIngreso;
+            }
+
+            set
+            {
+                idDetalleIngreso = value;
+            }
+        }
+
+        public int IdIngreso
+        {
+            get
+            {
+                return idIngreso;
+            }
+
+            set
+            {
+                idIngreso = value;
+            }
+        }
+
+        public int IdArticulo
+        {
+            get
+            {
+                return idArticulo;
+            }
+
+            set
+            {
+                idArticulo = value;
+            }
+        }
+
+        public decimal PrecioCompra
+        {
+            get
+            {
+                return precioCompra;
+            }
+
+            set
+            {
+                precioCompra = value;
+            }
+        }
+
+        public decimal PrecioVenta
+        {
+            get
+            {
+                return precioVenta;
+            }
+
+            set
+            {
+                precioVenta = value;
+            }
+        }
+
+        public int StockInicial
+        {
+            get
+            {
+                return stockInicial;
+            }
+
+            set
+            {
+                stockInicial = value;
+            }
+        }
+
+        public int StockActual
+        {
+            get
+            {
+                return stockActual;
+            }
+
+            set
+            {
+                stockActual = value;
+            }
+        }
+
+        public DateTime FechaProduccion
+        {
+            get
+            {
+                return fechaProduccion;
+            }
+
+            set
+            {
+                fechaProduccion = value;
+            }
+        }
+
+        public DateTime FechaVencimiento
+        {
+            get
+            {
+                return fechaVencimiento;
+            }
+
+            set
+            {
+                fechaVencimiento = value;
+            }
+        }
     }
 }
