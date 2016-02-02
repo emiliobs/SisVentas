7e5e38b38c5b20628f5ad612fc4f14b497920b4d
Emilio Barrera Sepúlveda <barrera_emilio@yahoo.es>
2016-02-02 22:52:36 +0100

    DIngresoDetalle - EnDesarrollo

---
+++ CapaPresentacion/frmLogin.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace CapaPresentacion
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.usuariotextBox = new System.Windows.Forms.TextBox();
+            this.PasswordtextBox = new System.Windows.Forms.TextBox();
+            this.Ingresarbutton = new System.Windows.Forms.Button();
+            this.Salirbutton = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.Horalabel = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.SlateBlue;
+            this.pictureBox1.Location = new System.Drawing.Point(-1, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(866, 110);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.SlateBlue;
+            this.pictureBox2.Location = new System.Drawing.Point(-1, 412);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(866, 47);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.SlateBlue;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(243, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(297, 36);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Sistema de  Ventas.";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.Horalabel);
+            this.groupBox1.Controls.Add(this.Salirbutton);
+            this.groupBox1.Controls.Add(this.Ingresarbutton);
+            this.groupBox1.Controls.Add(this.PasswordtextBox);
+            this.groupBox1.Controls.Add(this.usuariotextBox);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(275, 120);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(578, 286);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Acceso al Sistema.";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(50, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 29);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Usuario:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(50, 143);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(135, 29);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Password:";
+            // 
+            // usuariotextBox
+            // 
+            this.usuariotextBox.Location = new System.Drawing.Point(223, 71);
+            this.usuariotextBox.Name = "usuariotextBox";
+            this.usuariotextBox.Size = new System.Drawing.Size(300, 22);
+            this.usuariotextBox.TabIndex = 2;
+            // 
+            // PasswordtextBox
+            // 
+            this.PasswordtextBox.Location = new System.Drawing.Point(223, 143);
+            this.PasswordtextBox.Name = "PasswordtextBox";
+            this.PasswordtextBox.PasswordChar = '*';
+            this.PasswordtextBox.Size = new System.Drawing.Size(300, 22);
+            this.PasswordtextBox.TabIndex = 3;
+            // 
+            // Ingresarbutton
+            // 
+            this.Ingresarbutton.BackColor = System.Drawing.Color.SlateBlue;
+            this.Ingresarbutton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Ingresarbutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Ingresarbutton.ForeColor = System.Drawing.Color.White;
+            this.Ingresarbutton.Location = new System.Drawing.Point(223, 227);
+            this.Ingresarbutton.Name = "Ingresarbutton";
+            this.Ingresarbutton.Size = new System.Drawing.Size(126, 38);
+            this.Ingresarbutton.TabIndex = 4;
+            this.Ingresarbutton.Text = "Ingresar.";
+            this.Ingresarbutton.UseVisualStyleBackColor = false;
+            this.Ingresarbutton.Click += new System.EventHandler(this.Ingresarbutton_Click);
+            // 
+            // Salirbutton
+            // 
+            this.Salirbutton.BackColor = System.Drawing.Color.SlateBlue;
+            this.Salirbutton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Salirbutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Salirbutton.ForeColor = System.Drawing.Color.White;
+            this.Salirbutton.Location = new System.Drawing.Point(397, 227);
+            this.Salirbutton.Name = "Salirbutton";
+            this.Salirbutton.Size = new System.Drawing.Size(126, 38);
+            this.Salirbutton.TabIndex = 5;
+            this.Salirbutton.Text = "Salir.";
+            this.Salirbutton.UseVisualStyleBackColor = false;
+            this.Salirbutton.Click += new System.EventHandler(this.Salirbutton_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::CapaPresentacion.Properties.Resources.database_security;
+            this.pictureBox3.Location = new System.Drawing.Point(13, 120);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(246, 286);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 4;
+            this.pictureBox3.TabStop = false;
+            // 
+            // Horalabel
+            // 
+            this.Horalabel.AutoSize = true;
+            this.Horalabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Horalabel.Location = new System.Drawing.Point(219, 28);
+            this.Horalabel.Name = "Horalabel";
+            this.Horalabel.Size = new System.Drawing.Size(56, 20);
+            this.Horalabel.TabIndex = 6;
+            this.Horalabel.Text = "Hora:";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(865, 457);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmLogin";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox PasswordtextBox;
+        private System.Windows.Forms.TextBox usuariotextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button Salirbutton;
+        private System.Windows.Forms.Button Ingresarbutton;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label Horalabel;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ ProcedimientoAlmacenadosSisVentas/SQLQueryIngresocampoEstado.tblINgresosql.sql
@@ -0,0 +1,4 @@
+﻿--Agregar la columna estado a la tabla Ingreso:
+alter table ingreso
+add estado varchar(7) not null
+go
