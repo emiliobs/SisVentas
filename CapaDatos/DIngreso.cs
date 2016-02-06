4e94625787a4c36b31258d14e18771729b3b7e96
Emilio Barrera Sepúlveda <barrera_emilio@yahoo.es>
2016-02-06 16:08:59 +0100

    DIngreso y NIngreso -Finalizado

--- CapaDatos/DIngreso.cs
+++ CapaDatos/DIngreso.cs
@@ -155,6 +155,223 @@ namespace CapaDatos
             }
         }
 
+        //Metodos:
+        public string Insertar(DIngreso Ingreso, List<DDetalleIngreso> DetalleIngreso)
+        {
+            string respuesta = "";
+            SqlConnection SqlCon = new SqlConnection();
+            try
+            {
+                SqlCon.ConnectionString = Conexion.Cn;
+                SqlCon.Open();
+                //Establecer la transacción:
+                SqlTransaction SqlTransaccion = SqlCon.BeginTransaction();
+                //Establecer el Comando:
+                SqlCommand cmd = new SqlCommand("spInsertarIngreso", SqlCon);
+                cmd.Transaction = SqlTransaccion;
+                cmd.CommandType = CommandType.StoredProcedure;
+                //Parametros:
+                SqlParameter pIdIngreso = new SqlParameter("@IdIngreso", Ingreso.IdTrabajador);
+                cmd.Parameters.Add(pIdIngreso);
 
+                SqlParameter pIdtrabajador = new SqlParameter("@Idtrabajador", Ingreso.IdTrabajador);
+                cmd.Parameters.Add(pIdtrabajador);
+
+                SqlParameter pIdProveedor = new SqlParameter("@IdProveedor",Ingreso.IdProveedor);
+                cmd.Parameters.Add(pIdProveedor);
+
+                SqlParameter pFecha = new SqlParameter("@Fecha", Ingreso.Fecha);
+                cmd.Parameters.Add(pFecha);
+
+                SqlParameter pTipoComprobante = new SqlParameter("@TipoComprobante", Ingreso.TipoComprobante);
+                cmd.Parameters.Add(pTipoComprobante);
+
+                SqlParameter pSerie = new SqlParameter("@Serie", Ingreso.Serie);
+                cmd.Parameters.Add(pSerie);
+
+                SqlParameter pCorrelativo = new SqlParameter("@Correlativo", Ingreso.Correlativo);
+                cmd.Parameters.Add(pCorrelativo);
+
+
+                SqlParameter pIgv = new SqlParameter("@Igv", Ingreso.Igv);
+                cmd.Parameters.Add(pIgv);
+                
+                SqlParameter pEstado = new SqlParameter("@Estado", Ingreso.Estado);
+                cmd.Parameters.Add(pEstado);
+
+
+                //Ejecutamos nuestro comando:
+                respuesta = cmd.ExecuteNonQuery() == 1 ? "OK" : "No se ingres el Resgistro.....";
+
+                //Si pudo hacer el ingreso - entonces ingreso los detalles a ddetallesiINgreso:
+                if (respuesta.Equals("OK"))
+                {
+                    //Obtener el código del ingreso generado:
+                    this.IdIngreso = Convert.ToInt32(cmd.Parameters["@IdIngreso"].Value);
+
+                    foreach (DDetalleIngreso detalleIngresode  in DetalleIngreso)
+                    {
+                        detalleIngresode.IdIngreso = this.IdIngreso;
+
+                        //Llamar al método intertar de la la clase DDetalleIngreso:
+                        respuesta = detalleIngresode.Insertar(detalleIngresode,ref SqlCon, ref SqlTransaccion);
+
+                        if (!respuesta.Equals("OK"))
+                        {
+                            break;
+                        }
+                    }
+                }
+
+                //Evaluo la variable respuesta si es true o false:
+                if (respuesta.Equals("OK"))
+                {
+                    SqlTransaccion.Commit();
+                }
+                else
+                {
+                    SqlTransaccion.Rollback();
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                respuesta = "ERROR: " + ex.Message;
+            }
+            finally
+            {
+                if (SqlCon.State == ConnectionState.Open)
+                {
+                    SqlCon.Close();
+                }
+            }
+
+            return respuesta;
+        }
+
+        //Método Anular:  
+        public string Anular(DIngreso Ingreso)
+        {
+            string respuesta = string.Empty;
+            SqlConnection SqlCon = new SqlConnection();
+            try
+            {
+                SqlCon.ConnectionString = Conexion.Cn;
+                SqlCon.Open();
+                //Comando:
+                SqlCommand cmd = new SqlCommand("spAnularIngreso", SqlCon);
+                cmd.CommandType = CommandType.StoredProcedure;
+                //parametros:
+                SqlParameter pIdIngreso = new SqlParameter("@IdIngreso", Ingreso.IdIngreso);
+                cmd.Parameters.Add(pIdIngreso);
+
+                respuesta = cmd.ExecuteNonQuery() == 1 ? "OK" : "NO Se Anulo el Registro.....";
+
+            }
+            catch (Exception ex)
+            {
+
+                respuesta = "ERROR: " + ex.Message;
+            }
+            finally
+            {
+                if (SqlCon.State == ConnectionState.Open)
+                {
+                    SqlCon.Close();
+                }
+            }
+
+            return respuesta;
+        }
+
+        //Meétodo mostrar todos los valores de Ingreso:
+        public DataTable Mostrar()
+        {
+            DataTable dataTableResultado = new DataTable("ingreso");
+            SqlConnection sqlCon = new SqlConnection();
+            try
+            {
+                sqlCon.ConnectionString = Conexion.Cn;
+                sqlCon.Open();
+                //comando;
+                SqlCommand cmd = new SqlCommand("spMostrarIngreso", sqlCon);
+                cmd.CommandType = CommandType.StoredProcedure;
+                //dataAdapter
+                SqlDataAdapter Da = new SqlDataAdapter(cmd);
+                Da.Fill(dataTableResultado);
+            }
+            catch (Exception)
+            {
+
+                dataTableResultado = null;
+            }
+            sqlCon.Close();
+            return dataTableResultado;
+        }
+
+        //Método buscarfehas de Ingreso:
+        public DataTable BuscarIngresoFecha(string txtBuscar, string txtBuscar2)
+        {
+            DataTable dataTableResultado = new DataTable("ingreso");
+            SqlConnection sqlCon = new SqlConnection();
+            try
+            {
+                sqlCon.ConnectionString = Conexion.Cn;
+                sqlCon.Open();
+                //comando:
+                SqlCommand cmd = new SqlCommand("spBuscarIngresoFecha", sqlCon);
+                cmd.CommandType = CommandType.StoredProcedure;
+                //parametros:
+                SqlParameter ptxtBuscar = new SqlParameter("@txtBuscar", txtBuscar);
+                cmd.Parameters.Add(ptxtBuscar);
+
+                SqlParameter ptxtBuscar2 = new SqlParameter("@txtBuscar2", txtBuscar2);
+                cmd.Parameters.Add(ptxtBuscar2);
+                //DataAdaptar:
+                SqlDataAdapter Da = new SqlDataAdapter(cmd);
+                Da.Fill(dataTableResultado);
+
+            }
+            catch (Exception)
+            {
+
+                dataTableResultado = null;
+            }
+
+            sqlCon.Close();
+            return dataTableResultado;
+        }
+
+        //Método spMostrarDetalleIngreso
+        public DataTable MostrarDetalleIngreso(string txtBuscar)
+        {
+            DataTable dataTableResultado = new DataTable("ingreso");
+            SqlConnection sqlCon = new SqlConnection();
+            try
+            {
+                sqlCon.ConnectionString = Conexion.Cn;
+                sqlCon.Open();
+                //comando:
+                SqlCommand cmd = new SqlCommand("spMostrarDetalleIngreso", sqlCon);
+                cmd.CommandType = CommandType.StoredProcedure;
+                //parametros:
+                SqlParameter ptxtBuscar = new SqlParameter("@txtBuscar", txtBuscar);
+                cmd.Parameters.Add(ptxtBuscar);
+                              
+                //DataAdaptar:
+                SqlDataAdapter Da = new SqlDataAdapter(cmd);
+                Da.Fill(dataTableResultado);
+
+            }
+            catch (Exception)
+            {
+
+                dataTableResultado = null;
+            }
+
+            sqlCon.Close();
+            return dataTableResultado;
+        }
     }
 }
--- CapaNegocio/NIngreso.cs
+++ CapaNegocio/NIngreso.cs
@@ -3,10 +3,75 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//
+using CapaDatos;
+using System.Data;
 namespace CapaNegocio
 {
-    class NIngreso
-    {
+   public class NIngreso
+   {
+        //Método Insertar que llama al método insertar de la clase DIngreso de la CapaDatos:
+        public static string Insertat(int idtrabajador, int idproveedor, DateTime fecha,
+                                      string tipoComprobante, string serie, string correlativo,
+                                      decimal igv, string estado, DataTable dtDetalle )
+        {
+            DIngreso ingreso = new DIngreso();
+            ingreso.IdTrabajador = idtrabajador;
+            ingreso.IdProveedor = idproveedor;
+            ingreso.Fecha = fecha;
+            ingreso.TipoComprobante = tipoComprobante;
+            ingreso.Serie = serie;
+            ingreso.Correlativo = correlativo;
+            ingreso.Igv = igv;
+            ingreso.Estado = estado;
+
+            List<DDetalleIngreso> detalles = new List<DDetalleIngreso>();
+            //Recorro todas las filas una a una:
+            foreach (DataRow row in dtDetalle.Rows)
+            {
+                DDetalleIngreso detalle = new DDetalleIngreso();
+                detalle.IdArticulo = Convert.ToInt32(row["idarticulo"].ToString());
+                detalle.PrecioCompra = Convert.ToInt32(row["precio_compra"].ToString());
+                detalle.PrecioVenta = Convert.ToInt32(row["precio_venta"].ToString());
+                detalle.StockInicial = Convert.ToInt32(row["stock_inicial"].ToString());
+                detalle.StockActual = Convert.ToInt32(row["stock_actual"].ToString());
+                detalle.FechaProduccion = Convert.ToDateTime(row["fecha_produccion"].ToString());
+                detalle.FechaVencimiento = Convert.ToDateTime(row["fecha_vencimiento"].ToString());
+
+                detalles.Add(detalle);
+            }
+            return ingreso.Insertar(ingreso, detalles);
+        }
+
+
+        //Método Nular que llama al método Anular de la clase DIngreso de la CapaDatos:
+        public static string Eliminar(int idIngreso)
+        {
+            DIngreso ingreso = new DIngreso();
+            ingreso.IdIngreso = idIngreso;
+            return ingreso.Anular(ingreso);
+        }
+
+        //Método Mostrar que llama al método mostrar de la clase DIngreso  de la clase CapaDatos:
+        public static DataTable MostrarIngreso()
+        {
+            return new DIngreso().Mostrar();
+        }
+
+        //Método BuscarIngresoFecha que llama al método BuscarIngresoFecha  de la capa DIngreso de la CapaDATOS:
+        public static DataTable BuscarNombre(string txtBuscar, string txtBuscar2)
+        {
+            DIngreso ingreso = new DIngreso();
+
+            return ingreso.BuscarIngresoFecha(txtBuscar, txtBuscar2);
+        }
+
+        public static DataTable MostrarDetalleIngreso(string txtBuscar)
+        {
+            DIngreso ingreso = new DIngreso();
+
+            return ingreso.MostrarDetalleIngreso(txtBuscar);
+        }
+
     }
 }
